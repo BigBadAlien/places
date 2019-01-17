49809f06cf266b8bfc947bbd96ad68d5cdb44755
Aleksandr Ishchenko <tumpf0@gmail.com>
2019-01-18 02:14:34 +0300

    Save tables permanently

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,14 +1,30 @@
-import { Store, createStore, applyMiddleware } from 'redux';
+import { Store, applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { RootState, rootReducer } from 'app/reducers/index';
+import { RootState, rootReducer } from '../reducers';
 import { loggerMiddleware } from 'app/middleware/loggerMiddleware';
 import thunkMiddleware from 'redux-thunk';
+import { load, save } from "redux-localstorage-simple"
+import { initialState as placeInitialState } from '../reducers/place';
 
 export function configureStore(initialState?: RootState): Store<RootState> {
-  let middleware = applyMiddleware(loggerMiddleware, thunkMiddleware);
+  let middleware = applyMiddleware(
+    loggerMiddleware,
+    thunkMiddleware,
+    save({ states: ['place.places'], debounce: 500 }));
 
   if (process.env.NODE_ENV !== 'production') {
     middleware = composeWithDevTools(middleware);
+  }
+
+  const cache = load({ states: ['place.places']}) as Partial<RootState>;
+
+  if (cache && cache.place && cache.place.places) {
+    if (!initialState) {
+      initialState = {
+        place: placeInitialState
+      };
+    }
+    initialState.place.places = cache.place.places;
   }
 
   const store = createStore(rootReducer as any, initialState as any, middleware) as Store<RootState>;
