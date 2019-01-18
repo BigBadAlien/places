e2d201d93ae2c920c68bbef4b7e9b2cfa7e2803e
Aleksandr Ishchenko <tumpf0@gmail.com>
2019-01-18 13:59:55 +0300

    Convert GeoMap to stateless component

--- src/app/components/GeoMap/index.tsx
+++ src/app/components/GeoMap/index.tsx
@@ -2,40 +2,32 @@ import * as React from 'react';
 import { GoogleMap, Marker } from 'react-google-maps';
 import { MarkerData } from '../../models/MarkerData';
 
-export namespace PlaceAutocomplete {
-
-  export interface Props {
-    markers: MarkerData[];
-  }
-
-  export interface State {}
+export interface Props {
+  markers: MarkerData[];
 }
 
-export class GeoMap extends React.Component<PlaceAutocomplete.Props, PlaceAutocomplete.State> {
-  static defaultProps: Partial<PlaceAutocomplete.Props> = {};
-  static defaultCenter = { lat: 22.326442, lng: 114.167811 };
+const defaultProps: Partial<Props> = {};
+const defaultCenter = { lat: 22.326442, lng: 114.167811 };
 
-  constructor(props: PlaceAutocomplete.Props) {
-    super(props);
-  }
 
-  render() {
-    return (
-      <GoogleMap
-        defaultCenter={GeoMap.defaultCenter}
-        defaultZoom={8}
-        center={(this.props.markers[0] && this.props.markers[0].position) || GeoMap.defaultCenter}
-      >
-        {this.props.markers.map((marker, index) =>
-          <Marker key={index}
-                  position={marker.position}
-                  title={marker.title}
-                  icon={new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + marker.color,
-                    new google.maps.Size(21, 34),
-                    new google.maps.Point(0,0),
-                    new google.maps.Point(10, 34))}
-          />)}
-      </GoogleMap>
-    );
-  }
-}
+export const GeoMap: React.SFC<Props> = (props) => {
+  return (
+    <GoogleMap
+      defaultCenter={defaultCenter}
+      defaultZoom={10}
+      center={(props.markers[0] && props.markers[0].position) || defaultCenter}
+    >
+      {props.markers.map((marker, index) =>
+        <Marker key={index}
+                position={marker.position}
+                title={marker.title}
+                icon={new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + marker.color,
+                  new google.maps.Size(21, 34),
+                  new google.maps.Point(0,0),
+                  new google.maps.Point(10, 34))}
+        />)}
+    </GoogleMap>
+  );
+};
+
+GeoMap.defaultProps = defaultProps;
